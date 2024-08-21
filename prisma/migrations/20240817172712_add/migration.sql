5eaa2a13f84704ca1a0af90bad1145aafcb66611
Karan <karanmahatocse@gmail.com>
2024-08-21 11:53:41 +0530

    new changes

--- prisma/migrations/20240817172712_add/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "view_detection" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20240820072810_add/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `coordinates` on the `Camera` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Camera" DROP COLUMN "coordinates",
-ADD COLUMN     "coordinates" JSONB NOT NULL;
