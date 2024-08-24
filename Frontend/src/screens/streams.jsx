b90bb1b37ed9b0c2b83086f5a6543843d71e2247
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-08-24 17:08:57 +0530

    update

--- Frontend/src/screens/streams.jsx
+++ Frontend/src/screens/streams.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { GrPrevious } from "react-icons/gr";
 
 const StreamPage = () => {
   const itemsPerPage = 9; // Number of items per page
@@ -24,26 +25,26 @@ const StreamPage = () => {
     .map((_, index) => (
       <div
         key={index}
-        className="bg-white shadow-md rounded-lg h-64 w-[370px] flex items-center justify-center text-xl text-black font-bold"
+        className="bg-[#121212] shadow-md rounded-lg h-60 w-[400px] flex items-center justify-center text-xl text-white font-bold"
       >
         Box {index + 1 + (currentPage - 1) * itemsPerPage}
       </div>
     ));
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-black">
-      <div className="grid grid-cols-3 gap-4 p-4">
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-black relative">
+      <div className="grid grid-cols-3 gap-4 mb-[80px] ">
         {displayedItems}
       </div>
 
       {/* Pagination Controls */}
-      <div className=" flex items-center space-x-2 text-black ">
+      <div className= 'flex justify-center gap-3 items-center  text-black absolute bottom-10'>
         <button
           onClick={handlePrevious}
           className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
           disabled={currentPage === 1}
         >
-          Previous
+          <GrPrevious  className=''/>
         </button>
 
         <div className="flex items-center space-x-2">
@@ -51,7 +52,7 @@ const StreamPage = () => {
             <button
               key={index}
               onClick={() => setCurrentPage(index + 1)}
-              className={`px-4 py-2 rounded ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300 hover:bg-gray-400'}`}
+              className={`px-4 py-2 hover:-translate-y-1 ease-in-out duration-150 rounded-full ${currentPage === index + 1 ? 'bg-gray-800 text-white' : 'text-white font-semibold hover:bg-gray-900'}`}
             >
               {index + 1}
             </button>
@@ -63,7 +64,7 @@ const StreamPage = () => {
           className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
           disabled={currentPage === totalPages}
         >
-          Next
+          <GrPrevious  className='rotate-180'/>
         </button>
       </div>
     </div>
@@ -73,6 +74,4 @@ const StreamPage = () => {
 export default StreamPage;
 
 
-
-
-
+// {`flex items-center justify-center space-x-2 p-4 text-black ${totalItems <=3 ? 'absolute bottom-0 w-full bg-green-900': 'bg-yellow-700'}`}
