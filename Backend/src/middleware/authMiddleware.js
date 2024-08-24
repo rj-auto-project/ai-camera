ff8b48061a5a2e0876a727ac692b6cc2d722e566
Rishu Raj <rrishu561@gmail.com>
2024-08-24 19:55:06 +0530

    fix: prettier

--- Backend/src/middleware/authMiddleware.js
+++ Backend/src/middleware/authMiddleware.js
@@ -9,7 +9,6 @@ const authMiddleware = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded?.employee_Id;
-    console.log("Decoded:", decoded);
     next();
   } catch (err) {
     res.status(401).json({ error: "Invalid token" });
