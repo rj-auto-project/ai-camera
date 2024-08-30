9b9a7ea492cb2d47b27429afe427f76165f16472
Rishu Raj <rrishu561@gmail.com>
2024-08-30 15:42:47 +0530

    feat: suspect search operation

--- Frontend/src/components/form/SuspectSearchForm.jsx
+++ Frontend/src/components/form/SuspectSearchForm.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import TextFieldInput from "../Input/TextFieldInput";
 import SelectFieldInput from "../Input/SelectFieldInput";
-import { Button, Box } from "@mui/material";
+import { Button, Box, CircularProgress } from "@mui/material";
 import { useSuspectSearch } from "../../api/hooks/useSuspectSearch";
 import { formatDateTime } from "../../utils/formatTime";
+import SuspectSearchTable from "../table/SuspectSearchTable";
+import toast from "react-hot-toast";
 const SuspectSearchForm = ({ cameraList }) => {
   const {
     control,
@@ -40,7 +42,7 @@ const SuspectSearchForm = ({ cameraList }) => {
     { value: "bus", label: "Bus" },
     { value: "car", label: "Car" },
     { value: "child", label: "Child" },
-    { value: "Hatchback", label: "Hatchback" },
+    { value: "hatchback", label: "Hatchback" },
     { value: "helmet", label: "Helmet" },
     { value: "jcb", label: "JCB" },
     { value: "license-plate", label: "License Plate" },
@@ -65,6 +67,13 @@ const SuspectSearchForm = ({ cameraList }) => {
     { value: "woman", label: "Woman" },
   ];
 
+  // useEffect(() => {
+  //   setIsResultLoading(true);
+  //   const data = JSON.parse(sessionStorage.getItem("suspectSearchData"));
+  //   if (data) mutate(data);
+  //   setIsResultLoading(false);
+  // }, [mutate]);
+
   const onSubmit = (formData) => {
     console.log("Form data: ", formData);
     console.log("submiting", isLoading);
@@ -82,13 +91,26 @@ const SuspectSearchForm = ({ cameraList }) => {
     console.log("Formatted data: ", data);
 
     mutate(data);
+    toast.success("Suspect search form submitted successfully!", {
+      style: {
+        borderRadius: "10px",
+        background: "#333",
+        color: "#fff",
+        marginLeft: "4%",
+      },
+    });
+
+    //TODO: to be implemented
+    // sessionStorage.setItem("suspectSearchData", JSON.stringify(data));
   };
 
   console.log("susupect seach", data);
   console.log("isLoading", isLoading);
-  console.log("isError", isError,error);
+  console.log("isError", isError, error);
 
-  return (
+  return data ? (
+    <SuspectSearchTable data={data} />
+  ) : (
     <form onSubmit={handleSubmit(onSubmit)} style={{ padding: "16px" }}>
       <SelectFieldInput
         name="classes"
@@ -165,8 +187,11 @@ const SuspectSearchForm = ({ cameraList }) => {
         variant="contained"
         color="primary"
         style={{ marginTop: "16px", color: "white" }}
+        startIcon={
+          isLoading ? <CircularProgress size={20} color="inherit" /> : null
+        }
       >
-        Submit
+        {isLoading ? "creating..." : "Create"}
       </Button>
     </form>
   );
