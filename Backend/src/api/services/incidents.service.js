586aead073ae640b80bbb34609c4a003d5090df9
Rishu Raj <rrishu561@gmail.com>
2024-09-10 16:40:05 +0530

    modify: get garbages api

--- Backend/src/api/services/incidents.service.js
+++ Backend/src/api/services/incidents.service.js
@@ -1,10 +1,11 @@
 import prisma from "../../config/prismaClient.js";
 
 const detectGarbageService = async () => {
+  const classes = ["garbage", "pothole"];
   let results = await prisma.detectionLog.findMany({
     where: {
       detectionClass: {
-        equals: "garbage",
+        in: classes,
       },
     },
     distinct: ["trackId"],
