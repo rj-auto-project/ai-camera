347b82452c21e66f731a38980fc000aa68c2f182
Rishu Raj <rrishu561@gmail.com>
2024-09-15 17:15:44 +0530

    init: admin routes

--- Backend/src/utils/jwtToken.js
+++ Backend/src/utils/jwtToken.js
@@ -1,11 +1,15 @@
 import jwt from "jsonwebtoken";
 
 export async function generateToken(user) {
-  const { employee_Id, name } = user;
+  const { employee_Id, name, access_level } = user;
 
-  const token = jwt.sign({ employee_Id, name }, process.env.JWT_SECRET, {
+  const accessToken = jwt.sign({ employee_Id, name, access_level }, process.env.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = jwt.sign({ employee_Id, name, access_level }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
-  return token;
+  return { accessToken, refreshToken };
 }
