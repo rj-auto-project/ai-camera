75e49cde7e209ca211fbd39bb594b3ac219f3b1b
Rishu Raj <rrishu561@gmail.com>
2024-11-23 15:21:30 +0530

    feat: get surveys api

--- Backend/src/api/controllers/surveys.controller.js
+++ Backend/src/api/controllers/surveys.controller.js
@@ -1,4 +1,4 @@
-import { getSurveysAnalyticsService } from "../services/surveys.service.js";
+import { getPaginatedSurveysService, getSurveysAnalyticsService } from "../services/surveys.service.js";
 
 const getSurveysAnalytics = async (req, res) => {
   try {
@@ -17,4 +17,34 @@ const getSurveysAnalytics = async (req, res) => {
   }
 };
 
-export { getSurveysAnalytics };
+const getPaginatedSurveys = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const offset = (page - 1) * limit;
+
+    const surveys = await getPaginatedSurveysService(offset, limit);
+
+    if (surveys.length === 0) {
+      return res
+        .status(200)
+        .json({ status: "success", message: "No data found" });
+    }
+
+    return res.status(200).json({
+      page,
+      limit,
+      totalPages: Math.ceil(surveys.length / limit),
+      totalSurveys: surveys.length,
+      data: surveys,
+    })
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ status: "fail", message: "Internal server error" });
+  }
+}
+
+export { getSurveysAnalytics, getPaginatedSurveys };
--- Backend/src/api/routes/survey.routes.js
+++ Backend/src/api/routes/survey.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 import authMiddleware from "../../middleware/authMiddleware.js";
-import { getSurveysAnalytics } from "../controllers/surveys.controller.js";
+import { getPaginatedSurveys, getSurveysAnalytics } from "../controllers/surveys.controller.js";
 
 const router = express.Router();
 
 router.get("/analytics", authMiddleware, getSurveysAnalytics);
+router.get("/all", authMiddleware, getPaginatedSurveys);
 
 router.get("/status", (req, res) => {
   res.json({ status: "ok" });
