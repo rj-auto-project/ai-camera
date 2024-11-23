402d7e9d0c22f7ef25aecd35d07141ba41dec66f
Rishu Raj <rrishu561@gmail.com>
2024-11-23 15:38:52 +0530

    feat: get surveyreportsbysurveyid

--- Backend/src/api/routes/survey.routes.js
+++ Backend/src/api/routes/survey.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 import authMiddleware from "../../middleware/authMiddleware.js";
-import { getPaginatedSurveys, getSurveysAnalytics } from "../controllers/surveys.controller.js";
+import { getPaginatedSurveys, getSurveyReportsBySurveyId, getSurveysAnalytics } from "../controllers/surveys.controller.js";
 
 const router = express.Router();
 
 router.get("/analytics", authMiddleware, getSurveysAnalytics);
 router.get("/all", authMiddleware, getPaginatedSurveys);
+router.get("/reports/:id", authMiddleware, getSurveyReportsBySurveyId);
 
 router.get("/status", (req, res) => {
   res.json({ status: "ok" });
